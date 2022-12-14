cc0118adc572ff8e5297e7e9ce6be5d244f8feca
Muhammad Zam Zam Al Murshalat Adzan <adzan.zam@gmail.com>
2022-12-14 12:57:53 +0700

    created function test in api for test logic

--- app/Controllers/Api.php
+++ app/Controllers/Api.php
@@ -4,6 +4,18 @@ namespace App\Controllers;
 
 class Api extends BaseController
 {
+    public function test()
+    {
+        $requests = $this->barangModel->findAll();
+        // dd($requests);
+
+        $data = array();
+        foreach ($requests as $request) {
+            array_push($data, $request);
+        }
+
+        dd($data);
+    }
     public function index()
     {
         return view('welcome_message');
