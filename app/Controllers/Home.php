fa2cfcf92116774a2aa19599926928ac56a9c0a1
Naufal Ramadhan_20312022 <nauflrmdhan1234@gmail.com>
2022-12-14 12:13:50 +0700

    create controller home view barang

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -11,4 +11,27 @@ class Home extends BaseController
         ];
         return view('login', $data);
     }
+
+    public function barang()
+    {
+        $page = $this->request->getVar('page') ? $this->request->getVar('page') : 1;
+        $paginate = 5;
+
+        $search = $this->request->getVar('search');
+        if ($search) {
+            $barang = $this->barangModel->like('nama_barang', $search)->orLike('id_barang', $search);
+        } else {
+            $barang = $this->barangModel;
+        }
+
+        $data = [
+            'title' => 'Barang',
+            'barang' => $this->barangModel->findAll(),
+            // 'barang' => $barang->paginate($paginate),
+            'pager' => $this->barangModel->pager,
+            'page' => $page,
+            'paginate' => $paginate
+        ];
+        return session()->getTempdata('is_login') ? view('tambah_barang', $data) : redirect()->to(base_url());
+    }
 }
